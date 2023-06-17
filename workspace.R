949aa289ceb13e45766991d31e4378767cc32872
Faris Naji <faris.naji@tercen.com>
2023-06-17 09:16:29 +0000

    init the repo

--- workspace.R
+++ workspace.R
@@ -1,17 +1,67 @@
 library(tercen)
-library(dplyr, warn.conflicts = FALSE)
+library(tidyverse)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "2caf597dda19d36335786cffd100d360")
+options("tercen.stepId"     = "ebda5081-aae4-45fc-b91b-f7b4b19d402a")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
 ctx = tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>%
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+df <- ctx %>% rselect()
+
+
+df <- df %>% 
+  extract(col =1L, into = c("date"),      "([[:digit:]]+)", remove = FALSE)
+
+df <- df %>%   
+  extract(col =1L, into = c("well_id"),   "([ABCDEFGH][[:digit:]]+)", remove = FALSE) %>%
+  extract(col = "well_id",   into = "well_row",  regex = "([[:alpha:]]+)", remove =FALSE) %>%
+  extract(col = "well_id",   into = "well_col",  regex = "([[:digit:]]+)", remove =FALSE)
+
+df <- df %>%    
+  extract(col =1L, into = c("plate_id"),  "(Plate[[:alnum:]]+)",      remove = FALSE) %>% 
+  extract(col =1L, into = c("plate_id2"), "(Plate-[[:alnum:]]+)",     remove = FALSE) %>%
+  extract(col =1L, into = c("plate_id3"), "(plate[[:alnum:]]+)",      remove = FALSE) %>%
+  extract(col =1L, into = c("plate_id4"), "(Platte-[[:alnum:]]+)",    remove = FALSE) %>%
+  unite(col="plate_all", contains("plat"), sep = "", remove = TRUE, na.rm = TRUE)
+
+df <- df %>%   
+  extract(col = "plate_all", into = "plate_num", regex = "([[:digit:]]+)", remove =TRUE)
+
+well_row_key <-list()
+well_row_key['A'] <- 1
+well_row_key['B'] <- 2
+well_row_key['C'] <- 3
+well_row_key['D'] <- 4
+well_row_key['E'] <- 5
+well_row_key['F'] <- 6
+well_row_key['G'] <- 7
+well_row_key['H'] <- 8
+
+well_col_key <-list()
+well_col_key['01'] <- 1
+well_col_key['02'] <- 2
+well_col_key['03'] <- 3
+well_col_key['04'] <- 4
+well_col_key['05'] <- 5
+well_col_key['06'] <- 6
+well_col_key['07'] <- 7
+well_col_key['08'] <- 8
+well_col_key['09'] <- 9
+well_col_key['10'] <- 10
+well_col_key['11'] <- 11
+well_col_key['12'] <- 12
+
+df <- df  %>% mutate(well_row_num = unlist(well_row_key[well_row]))
+df <- df  %>% mutate(well_col_num = unlist(well_col_key[well_col]))
+
+df_out <- df %>%
+  select(-c(1))
+
+
+df_out %>% 
+  mutate(.ri = seq_len(nrow(.))-1L) %>%
   ctx$addNamespace() %>%
   ctx$save()
